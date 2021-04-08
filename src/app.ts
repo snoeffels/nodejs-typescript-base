198060f7efbe4985a663125289f25ee283b9b157
snoeffels <dennis.borrmann@strivecast.com>
2021-04-08 02:00:06 +0200

    - added template files

---
+++ src/app.ts
@@ -0,0 +1,4 @@
+console.log("hello from typescript :)");
+console.log("lemme see if this is working...");
+console.log("lemme see if this is working...");
+console.log("lemme see if this is working...");
